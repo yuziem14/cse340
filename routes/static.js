6f68c7927a57aa3ba132c18e251fbe07dae07522
Yuri Ziemba <yuri.fnunes@gmail.com>
2025-07-03 18:57:43 -0300

    add view files

--- routes/static.js
+++ routes/static.js
@@ -7,6 +7,9 @@ router.use(express.static("public"));
 router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
+router.get('/', (req, res) => {
+    res.render('layouts/layout', { body: '' });
+})
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -8,7 +8,15 @@
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
-const static = require("./routes/static")
+const static = require("./routes/static");
+const expressLayouts = require("express-ejs-layouts");
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', './layouts/layout');
 
 /* ***********************
  * Routes
