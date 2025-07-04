0ca013853dd8920bdfecafdb9ca9571093f91627
Yuri Ziemba <yuri.fnunes@gmail.com>
2025-07-04 03:19:43 -0300

    update landing page

--- routes/static.js
+++ routes/static.js
@@ -8,7 +8,7 @@ router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 router.get('/', (req, res) => {
-    res.render('layouts/layout', { body: '' });
+    res.render('index');
 })
 
 module.exports = router;
