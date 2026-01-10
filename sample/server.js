205b199b990fe278def644395cbe3cd1d7a25955
Yuri Ziemba <yuri.fnunes@gmail.com>
2026-01-10 04:02:20 -0300

    build a sample web server

---
+++ sample/server.js
@@ -0,0 +1,24 @@
+/* ******************************************
+ * This is the application server
+ * ******************************************/
+const express = require("express")
+
+const app = express()
+
+/* ******************************************
+ * Default GET route
+ * ***************************************** */
+app.get("/", (req, res) => {res.send("Welcome home!")})
+
+/* ******************************************
+ * Server host name and port
+ * ***************************************** */
+const HOST = 'localhost'
+const PORT = 3000
+
+/* ***********************
+* Log statement to confirm server operation
+* *********************** */
+app.listen(PORT, () => {
+console.log(`trial app listening on ${HOST}:${PORT}`)
+})
